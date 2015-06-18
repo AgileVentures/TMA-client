f3e898fb79dfab9c3279fd749d568e525b701abf
Razor <razorcd@yahoo.com>
2015-06-18 04:00:29 +0300

    added angular-stripe implementation

--- app/scripts/controllers/payment.js
+++ app/scripts/controllers/payment.js
@@ -7,7 +7,7 @@
  * # PaymentController
  * Controller of the tmaClientApp
  */
-app.controller('PaymentController', function ($scope, $routeParams, Order, AuthenticationService) {
+app.controller('PaymentController', function ($scope, $routeParams, Order, $window, CONFIG, AuthenticationService) {
   $scope.currentUser = AuthenticationService.currentUser();
   $scope.currentOrder;
 
@@ -25,5 +25,25 @@ app.controller('PaymentController', function ($scope, $routeParams, Order, Authe
       $scope.currentOrderError = "Order not found";
     }
   );
+
+  $window.Stripe.setPublishableKey(CONFIG.STRIPE_PUBLISHABLE_KEY);
+
+  // Stripe Response Handler
+  $scope.stripeCallback = function (code, result) {
+      if (result.error) {
+          window.alert('it failed! error: ' + result.error.message);
+      } else {
+          result.orderId = currentOrder.id;
+          //send stripe details to API
+          $http.post(CONFIG.BASE_URI + '/v1/payment', result)
+          .success(function(data){
+            console.log(data);
+          })
+          .error(function(errorMessage){
+              console.log(errorMessage);
+          });
+
+      }
+  };
 
 });
